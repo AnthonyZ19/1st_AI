e0240723e4e52b7fcb6cb7b3ccc2528f930e6582
Anthony <102531574+AnthonyZ19@users.noreply.github.com>
2023-05-23 13:31:30 -0400

    AI_Code.py

---
+++ AI_Code.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+I = [0.8, 0.54, 0.26, 0.92, 0.48]
+
+class NeuralNetworkL:
+    def __init__(self, inputs, neurons):
+        self.Weights = np.random.randn(inputs, neurons)*.10
+        self.Bias = np.random.randn(neurons)
+    def Weights_Bias(self, inputs, neurons):
+        self.Weights1 = np.random.randn(inputs, neurons)*.10
+        self.Bias1 = np.random.randn(neurons)
+    def Layer1(self, inputs):
+        Layer1Output = np.dot(inputs ,self.Weights) + self.Bias
+        return Layer1Output
+    def ReLU(self, inputs):
+        i = 0
+        while i < len(inputs):
+            if inputs[i] < 0:
+                inputs[i]=0
+            i += 1
+        return inputs
+    def SoftMax(inputs):
+        output = np.exp(inputs)/(np.exp(inputs).sum())
+        return output
+    def CrossEntropy(length, prediction, answer):
+        np.sum
+        
+
+NNL1 = NeuralNetworkL(len(I), 6)
+print("Weights: " + str(NNL1.Weights))
+print("Bias's: " + str(NNL1.Bias))
+print(NNL1.Layer1(I))
+print(NNL1.ReLU(NNL1.Layer1(I)))
+NNL2 = NeuralNetworkL(6,2)
+print("Weights 1: " + str(NNL2.Weights))
+print("Bias's 1: " + str(NNL2.Bias))
+L2O = NNL2.Layer1(NNL1.ReLU(NNL1.Layer1(I)))
+print(NNL2.Layer1(NNL1.ReLU(NNL1.Layer1(I))))
+print(NeuralNetworkL.SoftMax(L2O))
